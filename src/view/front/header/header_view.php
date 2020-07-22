bd1e6ece9fe5a334d0122e8f743e6d1c9adff119
Matthieu Clio <matthieu.clio@gmail.com>
2020-07-22 20:50:09 +0200

    Artlist V-0.2
    Ajout d'un menu

--- src/view/front/header/header_view.php
+++ src/view/front/header/header_view.php
@@ -1,8 +1,8 @@
-<base href="/callManager">
+<base href="/Artlist/">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>PhpBase</title>
+<title>Artlist</title>
 <meta name="description" content="Freamwork PHP">
-<link rel="icon" type="image/jpg" href="public/images/icone/">
+<link rel="icon" type="image/jpg" href="public/images/YourIcon/">
 <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
 <link rel="stylesheet" href="public/css/style.css">
---
+++ src/view/front/menu/menu_view.php
@@ -0,0 +1,7 @@
+<nav class="menu font-size-base">
+    barre de menu - icone recherche
+    <br>
+    Icon loupe
+    <br>
+    Start free now - princing sin In
+</nav>

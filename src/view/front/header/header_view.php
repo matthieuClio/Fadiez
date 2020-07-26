7d74eadf5a740f2f456044670dd0ac78bbc38ac6
Matthieu <matthieu.clio@gmail.com>
2020-07-26 21:30:57 +0200

    Artlist V-0.3
    Modification du menu

--- src/view/front/header/header_view.php
+++ src/view/front/header/header_view.php
@@ -4,5 +4,12 @@
 <title>Artlist</title>
 <meta name="description" content="Freamwork PHP">
 <link rel="icon" type="image/jpg" href="public/images/YourIcon/">
+
+<!-- Font-awesome -->
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+<!-- Normalize  -->
 <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
+
+<!-- Css custom -->
 <link rel="stylesheet" href="public/css/style.css">
--- src/view/front/menu/menu_view.php
+++ src/view/front/menu/menu_view.php
@@ -1,7 +1,11 @@
-<nav class="menu font-size-base">
-    barre de menu - icone recherche
-    <br>
-    Icon loupe
-    <br>
-    Start free now - princing sin In
+<nav class="menu-container font-size-default">
+    <span class="menu-options">
+        <i class="fa fa-bars menu-icon" aria-hidden="true"></i>
+        <p class="menu-text">Artlist</p>
+    </span>
+
+    <span class="connexion-options">
+        <input type="button" value="Start free trial" class="default-button">
+        <i class="fa fa-caret-down menu-icon" aria-hidden="true"></i>
+    </span>
 </nav>

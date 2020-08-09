9f3fb55909bcb730cf354ae4431406b5d4a2f0ba
Matthieu Clio <matthieu.clio@gmail.com>
2020-08-09 23:05:14 +0200

    Fadiez V-0.7
    Création du footer et création de la page princingView (page vierge pour le moment).

--- src/view/front/footer/footer_view.php
+++ src/view/front/footer/footer_view.php
@@ -1,3 +1,65 @@
 <footer class="main-footer">
-    <p>Footer (à venir)</p>
+    <figure class="footer-logo-container">
+        <img src="public/images/homePage/musiqueLector/test.jpg" alt="Logo" class="footer-logo"/>
+        <-LOGO A REMPLACER
+    </figure>
+
+    <section class="footer-about-container">
+        <h2 class="footer-about">À PROPOS</h2>
+        <p class="font-size-secondary-fact">
+            Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit. 
+            Cras tortor magna, porta eget efficitur vel,
+            pharetra nec turpis. 
+            Mauris mauris lectus, 
+            ultrices a condimentum vel, aliquet ac nisi.
+        </p>
+    </section>
+
+    <section class="footer-contact-container">
+        <h2 class="footer-contact">CONTACT</h2>
+        <p class="font-size-secondary-fact">
+            <span class="font-weight-bold-fact">
+                Numéro de téléphone :
+            </span> 
+            00 00 00 00 00
+            <br>
+            <span class="font-weight-bold-fact">
+                Adresse :
+            </span> 
+            13 rue de l'exemple
+            
+        </p>
+    </section>
+
+    <section class="footer-information-container">
+        <h2 class="footer-information">Informations supplémentaire</h2>
+        <p class="font-size-secondary-fact">
+            Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit. 
+            Cras tortor magna, porta eget efficitur vel,
+            pharetra nec turpis. 
+            Mauris mauris lectus, 
+            ultrices a condimentum vel, aliquet ac nisi.
+        </p>
+    </section>
+
+    <section class="footer-sitemap-container">
+        <h2 class="footer-sitemap">Plan du site</h2>
+        <p class="font-size-secondary-fact">
+            Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit. 
+            Cras tortor magna, porta eget efficitur vel,
+            pharetra nec turpis. 
+            Mauris mauris lectus, 
+            ultrices a condimentum vel, aliquet ac nisi.
+        </p>
+    </section>
+
+    <section class="footer-payement-container">
+        <h2 class="footer-payement">Payement</h2>
+        <p class="font-size-secondary-fact">
+            LOGO PAYPAL
+        </p>
+    </section>
 </footer>

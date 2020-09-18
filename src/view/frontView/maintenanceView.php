81313fdaafe831e8d7a61b4d4ca2fd57c581bc7f
Matthieu Clio <matthieu.clio@gmail.com>
2020-09-18 03:16:54 +0200

    Fadiez V-0.21
    Modification du text de la page de maintenance.

--- src/view/frontView/maintenanceView.php
+++ src/view/frontView/maintenanceView.php
@@ -7,7 +7,7 @@
 	<body>
 		<main style="display: flex; justify-content: center; align-items: center; flex-wrap: wrap; height: 100vh; background-color: #373737; color: white; font-size: 2em; text-align: center;">
 			<i class="fa fa-music" aria-hidden="true" style="width: 100%; font-size: 5em"></i>
-			Date de sortie prévu pour le site Fa-diez : 01/09/2020
+			Le site Fa-diez arrive Prochainement...
 		</main>
 	</body>
 </html>
